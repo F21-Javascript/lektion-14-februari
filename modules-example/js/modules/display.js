8342f8fe5012c662ad4ece077e3fbdcacdbd0ed6
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2022-02-14 11:54:42 +0100

    Added code

---
+++ modules-example/js/modules/display.js
@@ -0,0 +1,24 @@
+const trackListElem = document.querySelector('#track-list');
+const audioElem = document.querySelector('audio');
+
+function createListItem(track) {
+    const listItem = document.createElement('li');
+    listItem.innerText = track.name;
+    trackListElem.appendChild(listItem);
+
+    listItem.addEventListener('click', () => {
+        console.log('Preview url:', track.preview_url);
+        audioElem.setAttribute('src', track.preview_url);
+    });
+}
+
+function displayTracks(tracks) {
+    console.log(tracks);
+    for (const track of tracks) {
+        if (track.preview_url) {
+            createListItem(track);
+        }
+    }
+}
+
+export { displayTracks }
